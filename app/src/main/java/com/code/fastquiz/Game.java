9e0c55c6b9118d3b60de38138efd0abb66eef890
oscar <oscar.rivas.99@gmail.com>
2020-10-16 20:35:33 +0200

    put text to answers button from java

--- app/src/main/java/com/code/fastquiz/Game.java
+++ app/src/main/java/com/code/fastquiz/Game.java
@@ -2,9 +2,13 @@ package com.code.fastquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
@@ -15,26 +19,48 @@ public class Game extends AppCompatActivity {
 
         TextView question = findViewById(R.id.question_text);
         Button answer1 = findViewById(R.id.button_answer1);
+        Button answer2 = findViewById(R.id.button_answer2);
+        Button answer3 = findViewById(R.id.button_answer3);
+        Button answer4 = findViewById(R.id.button_answer4);
 
-
-        Question question1 = new Question("¿Como de tonto soy?");
-        question1.addAnswer("Mucho");
-        question1.addAnswer("Poco");
-        question1.addAnswer("Demasiado");
-        question1.setCorrect(1);
-        Question question2 = new Question("¿Vamos a aprobar?");
-        question2.addAnswer("Sí");
-        question2.addAnswer("No");
-        question2.addAnswer("Con ayuda de Jesucristo puede ser");
-        question2.setCorrect(1);
+        Question question1 = new Question("¿Cuál es la capital de Mongolia?");
+        question1.addAnswer("Estambul",false);
+        question1.addAnswer("Ulan Bator",true);
+        question1.addAnswer("Madrid",false);
+        question1.addAnswer("Tokio",false);
+        Question question2 = new Question("¿Quién escribió el Quijote?");
+        question2.addAnswer("Miguel de Unamuno",false);
+        question2.addAnswer("Federico García Lorca",false);
+        question2.addAnswer("Miguel de Cervantes",true);
+        question2.addAnswer("Gaspar Melchor de Jovellanos",false);
         Question question3 = new Question("¿Quién descubrió América?");
-        question3.addAnswer("Cristiano Ronaldo");
-        question3.addAnswer("Messi");
-        question3.addAnswer("Cristóbal Colón");
-        question3.setCorrect(3);
+        question3.addAnswer("Cristiano Ronaldo",false);
+        question3.addAnswer("Messi",false);
+        question3.addAnswer("Cristóbal Colón",true);
+        question3.addAnswer("Hernán Cortés",false);
+        Question question4 = new Question("¿Cuándo acabó la 2º Guerra Mundial?");
+        question4.addAnswer("1936",false);
+        question4.addAnswer("1938",false);
+        question4.addAnswer("1942",false);
+        question4.addAnswer("1945",true);
+        Question question5 = new Question("¿Cuál es el océano más grande del mundo?");
+        question5.addAnswer("Pacífico",true);
+        question5.addAnswer("Índico",false);
+        question5.addAnswer("Atlántico",false);
+        question5.addAnswer("Antártico",false);
 
         question.setText(question1.getQuestion());
-        //answer1.setText(question1.ge1);
+        answer1.setText(question1.getAnswer());
+        answer2.setText(question1.getAnswer());
+        answer3.setText(question1.getAnswer());
+        answer4.setText(question1.getAnswer());
+
+        answer1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Random rnd = new Random ();
+                System.out.println((int)(rnd.nextDouble()*3+1));
+            }
+        });
 
     }
 }
