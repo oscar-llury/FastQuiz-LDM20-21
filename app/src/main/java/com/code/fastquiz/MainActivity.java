34bd65b279ad79f752d1b5f4dd00615b6813ef8f
Carlos237 <48331360+Carlos237@users.noreply.github.com>
2020-10-16 18:24:30 +0200

    Starting question class and initializer
    Starting classes

--- app/src/main/java/com/code/fastquiz/MainActivity.java
+++ app/src/main/java/com/code/fastquiz/MainActivity.java
@@ -2,7 +2,11 @@ package com.code.fastquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        Question question1 = new Question("¿Como de tonto soy?");
+        question1.addAnswer("Mucho");
+        Question question2 = new Question("¿Vamos a aprobar?");
+        question2.addAnswer("Sí");
+                         }
+
+        }
+
