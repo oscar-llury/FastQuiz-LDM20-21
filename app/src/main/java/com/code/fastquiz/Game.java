0a83e44c00abe24bc0c4b408ab0c37b5f9642047
Carlos237 <48331360+Carlos237@users.noreply.github.com>
2020-10-20 19:48:39 +0200

    Random questions are now running and questions are showing colors of true/false

--- app/src/main/java/com/code/fastquiz/Game.java
+++ app/src/main/java/com/code/fastquiz/Game.java
@@ -17,10 +17,15 @@ public class Game extends AppCompatActivity {
     private Button answer3;
     private Button answer4;
     private Question question_to_show;
+    private ArrayList<Question> arrayQuestions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        // this.arrayQuestions = (ArrayList<Question>) getIntent().getSerializableExtra("arrayQuestions");
+        Initializer ini = new Initializer();
+        this.arrayQuestions = ini.getQuestion(5);
+
 
         TextView question = findViewById(R.id.question_text);
         answer1 = findViewById(R.id.button_answer1);
@@ -28,10 +33,7 @@ public class Game extends AppCompatActivity {
         answer3 = findViewById(R.id.button_answer3);
         answer4 = findViewById(R.id.button_answer4);
 
-        Initializer ini = new Initializer();
-        ArrayList<Question> arrayQuestions = ini.getQuestion(5);
-
-        Random rnd = new Random(2582054);
+        Random rnd = new Random(System.currentTimeMillis()*1000);
         question_to_show = arrayQuestions.get((int) (rnd.nextDouble()*arrayQuestions.size()));
         question.setText(question_to_show.getQuestion());
 
@@ -43,56 +45,51 @@ public class Game extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.button_answer1:{
+        switch (v.getId()) {
+            case R.id.button_answer1: {
                 boolean checking = question_to_show.checkCorrectAnswer(1);
-                if(checking) {
+                if (checking) {
                     answer1.setBackgroundColor(Color.GREEN);
-                }else{
+                } else {
                     answer1.setBackgroundColor(Color.RED);
                 }
-                answer1.setEnabled(false);
-                answer2.setEnabled(false);
-                answer3.setEnabled(false);
-                answer4.setEnabled(false);
+                break;
             }
-            case R.id.button_answer2:{
+            case R.id.button_answer2: {
                 boolean checking2 = question_to_show.checkCorrectAnswer(2);
-                if(checking2) {
+                if (checking2) {
                     answer2.setBackgroundColor(Color.GREEN);
-                }else{
+                } else {
                     answer2.setBackgroundColor(Color.RED);
                 }
-                answer1.setEnabled(false);
-                answer2.setEnabled(false);
-                answer3.setEnabled(false);
-                answer4.setEnabled(false);
+                break;
             }
-            case R.id.button_answer3:{
+            case R.id.button_answer3: {
                 boolean checking3 = question_to_show.checkCorrectAnswer(3);
-                if(checking3) {
+                if (checking3) {
                     answer3.setBackgroundColor(Color.GREEN);
-                }else{
+                } else {
                     answer3.setBackgroundColor(Color.RED);
                 }
-                answer1.setEnabled(false);
-                answer2.setEnabled(false);
-                answer3.setEnabled(false);
-                answer4.setEnabled(false);
+                break;
             }
-            case R.id.button_answer4:{
+            case R.id.button_answer4: {
                 boolean checking4 = question_to_show.checkCorrectAnswer(4);
-                if(checking4) {
+                if (checking4) {
                     answer4.setBackgroundColor(Color.GREEN);
-                }else{
+                } else {
                     answer4.setBackgroundColor(Color.RED);
                 }
-                answer1.setEnabled(false);
-                answer2.setEnabled(false);
-                answer3.setEnabled(false);
-                answer4.setEnabled(false);
+                break;
             }
         }
+            answer1.setEnabled(false);
+            answer2.setEnabled(false);
+            answer3.setEnabled(false);
+            answer4.setEnabled(false);
+            this.arrayQuestions.remove(question_to_show);
+            Intent activity = new Intent(getApplicationContext(),Game.class);
+            startActivity(activity);
 
     }
 }
