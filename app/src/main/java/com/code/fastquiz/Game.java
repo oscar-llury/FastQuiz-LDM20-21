950cd2216ed133f24dc301fd324a73ff1c52c2ea
Carlos237 <48331360+Carlos237@users.noreply.github.com>
2020-10-20 17:45:19 +0200

    Update Game.java

--- app/src/main/java/com/code/fastquiz/Game.java
+++ app/src/main/java/com/code/fastquiz/Game.java
@@ -24,41 +24,11 @@ public class Game extends AppCompatActivity {
         Button answer3 = findViewById(R.id.button_answer3);
         Button answer4 = findViewById(R.id.button_answer4);
 
-        ArrayList<Question> list_questions = new ArrayList<Question>();
-
-        Question question1 = new Question("¿Cuál es la capital de Mongolia?");
-        question1.addAnswer("Estambul",false);
-        question1.addAnswer("Ulan Bator",true);
-        question1.addAnswer("Madrid",false);
-        question1.addAnswer("Tokio",false);
-        Question question2 = new Question("¿Quién escribió el Quijote?");
-        question2.addAnswer("Miguel de Unamuno",false);
-        question2.addAnswer("Federico García Lorca",false);
-        question2.addAnswer("Miguel de Cervantes",true);
-        question2.addAnswer("Gaspar Melchor de Jovellanos",false);
-        Question question3 = new Question("¿Quién descubrió América?");
-        question3.addAnswer("Cristiano Ronaldo",false);
-        question3.addAnswer("Messi",false);
-        question3.addAnswer("Cristóbal Colón",true);
-        question3.addAnswer("Hernán Cortés",false);
-        Question question4 = new Question("¿Cuándo acabó la 2º Guerra Mundial?");
-        question4.addAnswer("1936",false);
-        question4.addAnswer("1938",false);
-        question4.addAnswer("1942",false);
-        question4.addAnswer("1945",true);
-        Question question5 = new Question("¿Cuál es el océano más grande del mundo?");
-        question5.addAnswer("Pacífico",true);
-        question5.addAnswer("Índico",false);
-        question5.addAnswer("Atlántico",false);
-        question5.addAnswer("Antártico",false);
-
-        list_questions.add(question1);
-        list_questions.add(question2);
-        list_questions.add(question3);
-        list_questions.add(question4);
+        Initializer ini = new Initializer();
+        ArrayList<Question> arrayQuestions = ini.getQuestion(5);
 
         Random rnd = new Random(2582054);
-        Question question_to_show = list_questions.get((int) (rnd.nextDouble()*list_questions.size()));
+        Question question_to_show = arrayQuestions.get((int) (rnd.nextDouble()*arrayQuestions.size()));
         question.setText(question_to_show.getQuestion());
 
         answer1.setText(question_to_show.getAnswer());
