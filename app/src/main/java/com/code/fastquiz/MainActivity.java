592bb66e60be22d780848d5b884a51237e7ab03c
oscar <oscar.rivas.99@gmail.com>
2020-11-16 20:46:13 +0100

    available change to dark mode on MainActivity

--- app/src/main/java/com/code/fastquiz/MainActivity.java
+++ app/src/main/java/com/code/fastquiz/MainActivity.java
@@ -1,14 +1,20 @@
 package com.code.fastquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -28,11 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
     private  RadioGroup question_group;
     @SuppressLint("UseSwitchCompatOrMaterialCode")
-    private Switch have_images;
+    private Switch have_images, switch1;
+    private boolean isNightModeEnabled;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.darkTheme);
+        }else{
+            setTheme(R.style.FastQuizTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button startButton = findViewById(R.id.button_start_game);
         question_group = findViewById(R.id.radioGroup_question);
         have_images = findViewById(R.id.switch_images);
@@ -40,6 +55,23 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setIcon(R.mipmap.ic_fastquiz);
         TextView textView8 = findViewById(R.id.textViewprueba);
         textView8.setText("prueba");
+
+        switch1=(Switch)findViewById(R.id.switch1);
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            switch1.setChecked(true);
+        }
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+                restart();
+            }
+        });
+
         startButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
 
@@ -67,6 +99,56 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(activity);
             }
         });
+    }
+
+    public void restart(){
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void checkTheme(){
+
+
+        //código que debe ir en ajustes
+        SharedPreferences prefs = getSharedPreferences("NIGHT_MODE", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("NIGHT_MODE", !this.isNightModeEnabled);
+        editor.commit();
+        //finish();
+        checkTheme();
+
+        /*
+        SharedPreferences prefs = getSharedPreferences("NIGHT_MODE", Context.MODE_PRIVATE);
+
+        this.isNightModeEnabled = prefs.getBoolean("NIGHT_MODE", false);
+
+*/
+/*
+        if (!this.isNightModeEnabled) {
+            Toast.makeText(getApplicationContext(), "LIGHT", Toast.LENGTH_SHORT).show();
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }else {
+            Toast.makeText(getApplicationContext(), "DARK", Toast.LENGTH_SHORT).show();
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }*/
+
+        /*
+        switch (AppCompatDelegate.getDefaultNightMode()) {
+            case AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM:
+                Toast.makeText(getApplicationContext(),"SYSTEM", Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case AppCompatDelegate.MODE_NIGHT_YES:
+                Toast.makeText(getApplicationContext(),"DARK", Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case AppCompatDelegate.MODE_NIGHT_NO:
+                Toast.makeText(getApplicationContext(),"LIGHT", Toast.LENGTH_SHORT).show();
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+        }
+*/
     }
 
     public void initRankingActivity(View view){
