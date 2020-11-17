e2636630f7d25de98cfbd689922d79227f28a306
Carlos <cgv237@gmail.com>
2020-11-17 18:58:47 +0100

    Added go back button at appbar

--- app/src/main/java/com/code/fastquiz/ConfigActivity.java
+++ app/src/main/java/com/code/fastquiz/ConfigActivity.java
@@ -1,12 +1,43 @@
 package com.code.fastquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class ConfigActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_config, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.help:
+                Intent manual = new Intent(this, ManualActivity.class);
+                startActivity(manual);
+                break;
+            case R.id.config:
+                Intent config = new Intent(this, ConfigActivity.class);
+                startActivity(config);
+                break;
+            case R.id.goback:
+                Intent atras = new Intent(this, MainActivity.class);
+                startActivity(atras);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +45,13 @@ public class ConfigActivity extends AppCompatActivity {
         Toolbar toolb =findViewById(R.id.app_bar);
         setSupportActionBar(toolb);
         toolb.setNavigationIcon(R.mipmap.ic_fastquiz);
+        toolb.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
+
 }
